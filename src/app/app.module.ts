85bb8d25dfcaddae8976b57d2bb1cd17b0cc165f
jayanth amirineni <jayasai.amerineni@gmail.com>
2018-11-24 12:00:37 +1100

    code base for client app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsService } from './services/forms.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FormsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
